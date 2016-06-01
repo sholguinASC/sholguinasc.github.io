dda6a6846cc67cd768916b4c258ec5fca4dd7dfb
sholguin <holguinsteven550@gmail.com>
2016-05-31 20:51:22 -0400

    Create giveaways.js

---
+++ giveaways.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+$("#p4").mouseenter(function(){
+    $("#p4_pic").show();
+});
+
+$("#p4").mouseleave(function(){
+    $("#p4_pic").hide();
+});
+$("#xone").mouseenter(function(){
+    $("#xone_pic").show();
+});
+
+$("#xone").mouseleave(function(){
+    $("#xone_pic").hide();
+});
+$("#6s").mouseenter(function(){
+    $("#6s_pic").show();
+});
+
+$("#6s").mouseleave(function(){
+    $("#6s_pic").hide();
+});
+});
