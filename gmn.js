98151f9cb305ee6bed080f3b74c357c613e6dc95
sholguin <holguinsteven550@gmail.com>
2016-06-06 12:43:33 -0400

    Create gmn.js

---
+++ gmn.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	var random_num = Math.floor((Math.random()*100) + 1);
+	//create click event for submit button
+
+
+
+	//create click event for clear button
+
+
+});
