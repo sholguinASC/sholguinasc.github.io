75fb04e854451d47b291bae973646bdd99320822
sholguin <holguinsteven550@gmail.com>
2016-06-07 12:03:46 -0400

    Update giveaways.js

--- giveaways.js
+++ giveaways.js
@@ -1,23 +1,23 @@
 $(document).ready(function(){
-$("#p4").mouseover(function(){
-        $("#p4_pic").css("display","block");
-        console.log("#p4 element mouseover");
-    });
-    $("#p4").mouseout(function(){
-        $("#p4_pic").css("display","none");
-    });
-    $("#xone").mouseover(function(){
-        $("#xone_pic").css("display","block");
-    });
-    
-    $("#xone").mouseout(function(){
-        $("#xone_pic").css("display","none");
-    });
-    $("#6s").mouseover(function(){
-        $("#iphone").css("display","block");
-    });
-    
-    $("#6s").mouseout(function(){
-        $("#iphone").css("display","none");
-    });
+        $("#p4").mouseover(function(){
+                $("#p4_pic").css("display","block");
+                console.log("#p4 element mouseover");
+        });
+        $("#p4").mouseout(function(){
+                $("#p4_pic").css("display","none");
+        });
+        $("#xone").mouseover(function(){
+                $("#xone_pic").css("display","block");
+        });
+            
+        $("#xone").mouseout(function(){
+                $("#xone_pic").css("display","none");
+        });
+        $("#6s").mouseover(function(){
+                $("#iphone").css("display","block");
+        });
+            
+        $("#6s").mouseout(function(){
+                $("#iphone").css("display","none");
+        });
 })
