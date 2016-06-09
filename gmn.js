82a6985e328ece3a2d81894b98f632332a4013ee
sholguin <holguinsteven550@gmail.com>
2016-06-08 23:01:45 -0400

    Update gmn.js

--- gmn.js
+++ gmn.js
@@ -1,10 +1,34 @@
-$(document).ready(function(){
-	var random_num = Math.floor((Math.random()*100) + 1);
+//$(document).ready(function(){
+//	var random_num = Math.floor((Math.random()*100) + 1);
 	//create click event for submit button
-
-
-
+//
+//
+//
 	//create click event for clear button
-
-
+//
+//
+//});
+$(document).ready(function(){
+  //find a way for the program to choose a random number between 1 and 100, save this as a variable
+  
+  
+  //when the player clicks on the 'guess' button
+  $('button').on('click', function(){
+    //save their guess as a variable
+    
+    
+    //and compare this guess to the random number that the computer picked
+    //if the user guessed the correct number...
+    
+      //what happens if the guess is correct?
+      
+    //if the user guessed too high...
+    
+      //update the 'feedback' paragraph to tell them to guess lower
+      
+    //otherwise, the user guessed too low...
+    
+      //update the 'feedback' paragraph to tell them to guess higher
+    
+  });
 });
