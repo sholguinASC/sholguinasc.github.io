0b8d8f23b0649fa6aa03aedd215eea340a4e9592
sholguin <holguinsteven550@gmail.com>
2016-06-09 12:43:42 -0400

    Update giveaways.js

--- giveaways.js
+++ giveaways.js
@@ -1,23 +1,28 @@
 $(document).ready(function(){
         $("#p4").mouseover(function(){
                 $("#p4_pic").css("display","block");
-                console.log("#p4 element mouseover");
+                $("body").css("background-color","#00b2ee");
         });
         $("#p4").mouseout(function(){
                 $("#p4_pic").css("display","none");
+                $("body").css("background-color","#ff6666");
         });
         $("#xone").mouseover(function(){
                 $("#xone_pic").css("display","block");
+                $("body").css("background-color","#00aa50");
         });
             
         $("#xone").mouseout(function(){
                 $("#xone_pic").css("display","none");
+                $("body").css("background-color","#ff6666");
         });
         $("#6s").mouseover(function(){
                 $("#iphone").css("display","block");
+                $("body").css("background-color","#5f68a3");
         });
             
         $("#6s").mouseout(function(){
                 $("#iphone").css("display","none");
+                $("body").css("background-color","#ff6666");
         });
 })
