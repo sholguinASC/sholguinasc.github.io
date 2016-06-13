d312ef0bb48a6dea0c0568449dd19e21b2b2a817
sholguin <holguinsteven550@gmail.com>
2016-06-12 23:51:11 -0400

    Update gmn.js

--- gmn.js
+++ gmn.js
@@ -7,6 +7,22 @@ $(document).ready(function(){
     		$("#guess_list").append(response);
     		$('#guess').val('');
 	});
+	if (guess == random_num) {
+              $("#response").append("<p>"+ first +"</p>");
+              $("#response").append("<h1>Good Job you guessed my number!</h1>");
+              $("#response").delay(100).append("<h3>Do you want a cookie??</h3>");
+              $("#response").css("background-color","#00CD00");
+            } 
+        else if (guess < random_num) {
+                $("#response").append("<p>"+ first +"</p>");
+                $("#response").append("<h1>Your Guess is too low</h1>");
+                $("#response").css("background-color","#FF3232");
+            } 
+        else {
+                  $("#response").append("<p>"+ first +"</p>");
+                  $("#response").append("<p>Your Guess is too high</p>");
+                  $("#response").css("background-color","#FF3232");
+            }
 	
 });
 
