87d3e6aeed785bfb7886257ba5eeac03c9bb8c48
sholguin <holguinsteven550@gmail.com>
2016-06-13 00:20:56 -0400

    Update gmn.js

--- gmn.js
+++ gmn.js
@@ -1,32 +1,31 @@
 $(document).ready(function(){
 	$("#response").empty();
 	var random_num = Math.floor((Math.random()*100) + 1);
-	var first = $("#guess").val();
 	//create click event for submit button
 	$("#submit").click(function(){
-    		var two = $("#guess").val();
-    		var response= "<p>" + two + "</p>";
+    		var first = $("#guess").val();
+    		var response= "<p>" + first + "</p>";
     		$("#guess_list").append(response);
     		$('#guess').val('');
 		if (first == random_num) {
-				$("#response").empty();
-		              $("#response").append("<p>"+ first +"</p>");
-		              $("#response").append("<h1>Good Job you guessed my number!</h1>");
-		              $("#response").delay(100).append("<h3>Do you want a cookie??</h3>");
-		              $("#response").css("background-color","#00CD00");
-		            } 
-		  else if (first < random_num) {
-		        	$("#response").empty();
-		                $("#response").append("<p>"+ first +"</p>");
-		                $("#response").append("<h1>Your Guess is too low</h1>");
-		                $("#response").css("background-color","#FF3232");
-		            } 
-		        else {
-		        	$("#response").empty();
-		                  $("#response").append("<p>"+ first +"</p>");
-		                  $("#response").append("<h1>Your Guess is too high</h1>");
-		                  $("#response").css("background-color","#FF3232");
-		            }
+			$("#response").empty();
+		        $("#response").append("<h6>"+ first +"</h6>");
+		        $("#response").append("<h1>Good Job you guessed my number!</h1>");
+		        $("#response").delay(100).append("<h3>Do you want a cookie??</h3>");
+		        $("#response").css("background-color","#00CD00");
+                 } 
+		 else if (first < random_num) {
+		       	$("#response").empty();
+		        $("#response").append("<h6>"+ first +"</h6>");
+	                $("#response").append("<h1>Your Guess is too low</h1>");
+		        $("#response").css("background-color","#FF3232");
+                 } 
+	         else {
+		     	$("#response").empty();
+	                $("#response").append("<h6>"+ first +"</h6>");
+		        $("#response").append("<h1>Your Guess is too high</h1>");
+		        $("#response").css("background-color","#FF3232");
+	         }
 	
 	});
 });
