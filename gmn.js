10fff79fa3967f41f261b9e8102008306e558a74
sholguin <holguinsteven550@gmail.com>
2016-06-12 22:27:00 -0400

    Update gmn.js

--- gmn.js
+++ gmn.js
@@ -6,7 +6,6 @@ $(document).ready(function(){
     		var response= "<li>first"</li>";
     		$("#guesses").append(response);
     		$('#guess').val('');
-  });
 	$('.input').keypress(function(e) {
         	if(e.which == 13) {
             		jQuery(this).blur();
