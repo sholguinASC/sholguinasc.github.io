9ba2497dc2e47798868927286d8d18003c210347
sholguin <holguinsteven550@gmail.com>
2016-06-12 23:17:58 -0400

    Update gmn.js

--- gmn.js
+++ gmn.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
 	$("#submit").click(function(){
     		var first = $("#guess").val();
     		var response= "<p>" + first + "</p>";
-    		$("#guesses").append(response);
-    		$('#guess_list').val('');
+    		$("#guess_list").append(response);
+    		$('#guess').val('');
 	});
 	
 });
