b9a6348d34052c3c56fc73b9e05a7e4c694ed069
sholguin <holguinsteven550@gmail.com>
2016-06-12 21:55:00 -0400

    Update gmn.js

--- gmn.js
+++ gmn.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
-var random_num = Math.floor((Math.random()*100) + 1);
+	var random_num = Math.floor((Math.random()*100) + 1);
 	//create click event for submit button
-
+	$("#submit").click(function(){
+    		var first = $("#guess").val();
+    		var response= "<p>Guess:" + first + "</p>";
+    	$("#guesses").append(response);
+  });
+});
 
 
 	//create click event for clear button
 
 
-});
 
