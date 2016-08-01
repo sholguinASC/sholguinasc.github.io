f708239ebc282397d4075ac1430e5fd1516e7e7d
sholguinASC <holguinsteven550@gmail.com>
2016-08-01 15:29:58 -0400

    Update ttt.js

--- ttt.js
+++ ttt.js
@@ -6,11 +6,11 @@ function draw(){
   background(155,255,200)
   var i
   var a
-  for (i = 0; i>3; i++){
-    for (a = 0; a>3; a++){
+  for (i = 0; i<3; i++){
+    for (a = 0; a<3; a++){
       fill(255)
       background(200);
-      rect([a * 200],[i * 1],200,200)
+      rect([a * 200],[i * 1],199,199)
     }
   }
 }
