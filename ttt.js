0ab097ae660e7e1e6b8c47c053abe7f7ea9bf1b3
sholguinASC <holguinsteven550@gmail.com>
2016-08-01 11:37:17 -0400

    Update ttt.js

--- ttt.js
+++ ttt.js
@@ -1,8 +1,12 @@
 function setup(){
-  createCanvas(700, 700, WEBGL);
+  createCanvas(600, 600, WEBGL);
 }
 
 function draw(){
-  background(200);
-  plane(200, 200);
+  var i
+  var a
+  for (i = 0; i==3; i++){
+    for (a = 0; a==3; a++){
+      background(200);
+      plane(200,200,[a * 200],[i * 1])
 }
