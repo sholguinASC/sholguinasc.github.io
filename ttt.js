fef9ae19e37ab55ee5071b61fce58a02f9dfb559
sholguinASC <holguinsteven550@gmail.com>
2016-08-02 09:19:49 -0400

    Update ttt.js

--- ttt.js
+++ ttt.js
@@ -1,15 +1,15 @@
-function checkPosition(a,c){
-    if list[a][c] == 1{
-        fill(200,55,123)
-        ellipse(a*100 + 50,c*100 + 50,100,100)
-        win = 1
-    }
-    else{
-        fill(255,0,0)
-        rect(a*100,c*100,100,100) 
-        count = count + 1
-    }
-}
+//function checkPosition(a,c){
+//    if list[a][c] == 1{
+//        fill(200,55,123)
+//        ellipse(a*100 + 50,c*100 + 50,100,100)
+//        win = 1
+//    }
+//    else{
+//        fill(255,0,0)
+//        rect(a*100,c*100,100,100) 
+//        count = count + 1
+//    }
+//}
 
 function setup(){
   createCanvas(600, 600);
