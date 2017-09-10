54d0e783bd59c0cf8755a6b159b26ce8a452051c
sholguinASC <holguinsteven550@gmail.com>
2017-09-10 02:00:39 -0400

    Create anabel19992309.js

---
+++ anabel19992309.js
@@ -0,0 +1,53 @@
+window.onload = function(){
+  var debug = /*true ||*/ false;
+  var h = document.querySelector('.heart-wrapper');
+  
+  function toggleActivate(){
+    h.classList.toggle('active');
+  }
+
+  if(!debug){
+    h.addEventListener('click',function(){
+      toggleActivate();
+    },false);
+
+    // setInterval(toggleActivate,1000);
+  }else{
+    var elts = Array.prototype.slice.call(h.querySelectorAll(':scope > *'),0);
+    var activated = false;
+    var animating = false;
+    var count = 0;
+    var step = 1000;
+    
+    function setAnim(state){
+      elts.forEach(function(elt){
+        elt.style.animationPlayState = state;
+      });
+    }
+    
+    h.addEventListener('click',function(){
+      if (animating) return;
+      if (count > 27) {
+        h.classList.remove('active');
+        count = 0;
+        return;
+      }
+      if (!activated) h.classList.add('active') && (activated = true);
+      
+      console.log('Step : '+(++count));
+      animating = true;
+      
+      setAnim('running');
+      setTimeout(function(){
+        setAnim('paused');
+        animating = false;
+      },step);
+    },false);
+
+    setAnim('paused');
+    elts.forEach(function(elt){
+      elt.style.animationDuration = step/1000*27+'s';
+    });
+  }
+  
+};
