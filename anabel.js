0e32999781560540c045235ff1c271adf1d73e58
sholguinASC <holguinsteven550@gmail.com>
2017-09-13 00:18:07 -0400

    Update anabel.js

--- anabel.js
+++ anabel.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
-	var correct_code = 00000000;
+	var correct_code = "00000000";
         var response= "<center><h1>No Bitch Your Wrong</h1></center>";
 	var tryAgain = "<center><h5>Try again my fren</h5></center>";
 	//create click event for submit button
 	$("#submit").click(function(){
     		var first = $("#code").val();
-		if (first == correct_code) {
+		var second = first.toString();
+		if (second == correct_code) {
 		        window.location = "sholguinasc.github.io/anabel.html/#";
                  } 
 	         else {
