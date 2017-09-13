bd697065a552f72087120133c5f2c158bd76a8dc
sholguinASC <holguinsteven550@gmail.com>
2017-09-13 00:09:03 -0400

    Update anabel.js

--- anabel.js
+++ anabel.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	var correct_code = 00000000;
-        var response= "<h1>No Bitch Your Wrong</h1>";
+        var response= "<center><h1>No Bitch Your Wrong</h1></center>";
 	//create click event for submit button
 	$("#submit").click(function(){
     		var first = $("#code").val();
@@ -9,7 +9,7 @@ $(document).ready(function(){
                  } 
 	         else {
 		     	$("#top_text").replaceWith(response);
-          		$("body").css("background-color","red");
+          		$("body").css("background-color","#f71b1b");
 	         }
 	
 	});
