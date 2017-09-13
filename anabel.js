a6df6c8db1a0dee413017cee6272021d45788434
sholguinASC <holguinsteven550@gmail.com>
2017-09-13 00:13:47 -0400

    Update anabel.js

--- anabel.js
+++ anabel.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	var correct_code = 00000000;
         var response= "<center><h1>No Bitch Your Wrong</h1></center>";
+	var tryAgain = "<center><h5>Try again my fren</h5></center>";
 	//create click event for submit button
 	$("#submit").click(function(){
     		var first = $("#code").val();
@@ -10,6 +11,8 @@ $(document).ready(function(){
 	         else {
 		     	$("#top_text").replaceWith(response);
           		$("body").css("background-color","#f71b1b");
+			$( "h5" ).replaceWith(tryAgain);
+ 
 	         }
 	
 	});
