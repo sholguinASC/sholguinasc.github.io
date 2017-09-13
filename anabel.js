13235ffdde81e975cbba19b6b650e390ae231ab3
sholguinASC <holguinsteven550@gmail.com>
2017-09-13 00:04:05 -0400

    Update anabel.js

--- anabel.js
+++ anabel.js
@@ -1,16 +1,15 @@
 $(document).ready(function(){
 	var correct_code = 00000000;
-  var response= "<h1>No Bitch Your Wrong</h1>";
+        var response= "<h1>No Bitch Your Wrong</h1>";
 	//create click event for submit button
 	$("#submit").click(function(){
     		var first = $("#code").val();
 		if (first == correct_code) {
-			$("#response").empty();
-		        $("#response").append("<h6>"+ first +"</h6>");
+		        window.location = "sholguinasc.github.io/anabel19992309.html";
                  } 
 	         else {
 		     	$("#top_text").replaceWith(response);
-          $("#response").css("background-color","#00CD00");
+          		$("body").css("background-color","red");
 	         }
 	
 	});
